311bfe089ea83976160bfd892d6fe584de7672ec
Doridian <git@doridian.net>
2024-10-12 01:05:59 -0700

    rename

--- .github/container/build.sh
+++ .github/container/build.sh
@@ -5,16 +5,16 @@ export HOME=/home/user
 
 mkdir -p /home/user/.abuild
 if [ ! -z "${ABUILD_PRIVATE_KEY-}" ]; then
-    echo "$ABUILD_PRIVATE_KEY" > "/home/user/.abuild/abuild.key"
-    chmod 600 "/home/user/.abuild/abuild.key"
+    echo "$ABUILD_PRIVATE_KEY" > "/home/user/.abuild/libnss_igshim_ci.key"
+    chmod 600 "/home/user/.abuild/libnss_igshim_ci.key"
 fi
 
 if [ ! -z "${ABUILD_PUBLIC_KEY-}" ]; then
-    echo "$ABUILD_PUBLIC_KEY" > "/home/user/.abuild/abuild.key.pub"
-    sudo cp -fv /home/user/.abuild/abuild.key.pub /etc/apk/keys/
+    echo "$ABUILD_PUBLIC_KEY" > "/home/user/.abuild/libnss_igshim_ci.key.pub"
+    sudo cp -fv /home/user/.abuild/libnss_igshim_ci.key.pub /etc/apk/keys/
 fi
 
-echo 'PACKAGER_PRIVKEY="/home/user/.abuild/abuild.key"' > /home/user/.abuild/abuild.conf
+echo 'PACKAGER_PRIVKEY="/home/user/.abuild/libnss_igshim_ci.key"' > /home/user/.abuild/abuild.conf
 echo "PACKAGER=\"${GIT_USER_NAME} <${GIT_USER_EMAIL}>\"" >> /home/user/.abuild/abuild.conf
 echo 'REPODEST="/src/packages"' >> /home/user/.abuild/abuild.conf
 
