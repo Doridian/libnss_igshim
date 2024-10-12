9c55349f6ba4df70af13563328c12dbf7f734b4b
Doridian <git@doridian.net>
2024-10-11 18:02:06 -0700

    fmt

--- libnss_igshim.c
+++ libnss_igshim.c
@@ -12,11 +12,11 @@
 
 enum nss_status
 {
-	NSS_STATUS_TRYAGAIN = -2,
-	NSS_STATUS_UNAVAIL = -1,
-	NSS_STATUS_NOTFOUND = 0,
-	NSS_STATUS_SUCCESS = 1,
-	NSS_STATUS_RETURN = 2
+    NSS_STATUS_TRYAGAIN = -2,
+    NSS_STATUS_UNAVAIL = -1,
+    NSS_STATUS_NOTFOUND = 0,
+    NSS_STATUS_SUCCESS = 1,
+    NSS_STATUS_RETURN = 2
 };
 
 typedef void t_mod_setgrent(void);
@@ -30,31 +30,31 @@ t_mod_getgrent_r *nss_mod_getgrent_r;
 // get_dll from: https://github.com/pikhq/musl-nscd/blob/master/src/main.c
 static void *get_dll(const char *service)
 {
-	char *path;
-	void *dll;
-	if(asprintf(&path, "libnss_%s.so.2", service) < 0) exit(1);
-	dll = dlopen(path, RTLD_NOW | RTLD_LOCAL);
-	if(!dll) {
-		sprintf(path, "libnss_%s.so", service);
-		dll = dlopen(path, RTLD_NOW | RTLD_LOCAL);
-	}
-	if(!dll) {
+    char *path;
+    void *dll;
+    if(asprintf(&path, "libnss_%s.so.2", service) < 0) exit(1);
+    dll = dlopen(path, RTLD_NOW | RTLD_LOCAL);
+    if(!dll) {
+        sprintf(path, "libnss_%s.so", service);
+        dll = dlopen(path, RTLD_NOW | RTLD_LOCAL);
+    }
+    if(!dll) {
         printf("%s: %s", path, dlerror());
         exit(1);
     }
-	free(path);
-	return dll;
+    free(path);
+    return dll;
 }
 
 // get_fn from: https://github.com/pikhq/musl-nscd/blob/master/src/main.c
 static void *get_fn(void *dll, const char *name, const char *service)
 {
-	char *fnname;
-	void *fn;
-	if(asprintf(&fnname, "_nss_%s_%s", service, name) < 0) exit(1);
-	fn = dlsym(dll, fnname);
-	free(fnname);
-	return fn;
+    char *fnname;
+    void *fn;
+    if(asprintf(&fnname, "_nss_%s_%s", service, name) < 0) exit(1);
+    fn = dlsym(dll, fnname);
+    free(fnname);
+    return fn;
 }
 
 __attribute__((constructor))
@@ -88,57 +88,57 @@ void igshim_module_init()
 
 enum nss_status _nss_igshim_initgroups_dyn(const char* username, gid_t gid, long* start, long* size, gid_t** groupsp, long limit, int *errorp)
 {
-	if (gid != -1) {
-		return NSS_STATUS_NOTFOUND;
-	}
+    if (gid != -1) {
+        return NSS_STATUS_NOTFOUND;
+    }
 
-	int res;
-	char buf[4096];
-	struct group grp;
-	struct group *grpp;
-	long entries;
+    int res;
+    char buf[4096];
+    struct group grp;
+    struct group *grpp;
+    long entries;
 
-	char **members;
+    char **members;
 
-	nss_mod_setgrent();
+    nss_mod_setgrent();
 
-	while(entries < limit) {
-		res = nss_mod_getgrent_r(&grp, buf, sizeof(buf), &grpp);
+    while(entries < limit) {
+        res = nss_mod_getgrent_r(&grp, buf, sizeof(buf), &grpp);
         if (!res) {
             break;
         }
-		if (errno && errno != EAGAIN) {
-			perror("mod_getgrent_r");
-			nss_mod_endgrent();
+        if (errno && errno != EAGAIN) {
+            perror("mod_getgrent_r");
+            nss_mod_endgrent();
             return NSS_STATUS_TRYAGAIN;
-		}
+        }
 
 
-		if (*size == *start) {
-			*size *= 2;
-			*groupsp = realloc(*groupsp, *size * sizeof(gid_t));
-			if (!*groupsp) {
-				*errorp = ENOMEM;
-				nss_mod_endgrent();
-				return NSS_STATUS_TRYAGAIN;
-			}
-		}
+        if (*size == *start) {
+            *size *= 2;
+            *groupsp = realloc(*groupsp, *size * sizeof(gid_t));
+            if (!*groupsp) {
+                *errorp = ENOMEM;
+                nss_mod_endgrent();
+                return NSS_STATUS_TRYAGAIN;
+            }
+        }
 
-		for (members = grp.gr_mem; *members != NULL; members++) {
-			if (strcmp(username, *members) == 0) {
-				break;
-			}
-		}
+        for (members = grp.gr_mem; *members != NULL; members++) {
+            if (strcmp(username, *members) == 0) {
+                break;
+            }
+        }
 
-		if (*members == NULL) {
-			continue;
-		}
+        if (*members == NULL) {
+            continue;
+        }
 
-		(*groupsp)[*start] = grp.gr_gid;
-		(*start)++;
-		entries++;
-	}
+        (*groupsp)[*start] = grp.gr_gid;
+        (*start)++;
+        entries++;
+    }
 
-	nss_mod_endgrent();
-	return NSS_STATUS_SUCCESS;
+    nss_mod_endgrent();
+    return NSS_STATUS_SUCCESS;
 }
